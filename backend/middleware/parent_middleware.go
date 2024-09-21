4e113fd0d40fa56aafc6d376b848f8532995b64b
Enkutatash <enkutatasheshetu96@gmail.com>
2024-09-21 12:19:11 +0300

    [middleware] for all users

--- backend/middleware/parent_middleware.go
+++ backend/middleware/parent_middleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	
 	"net/http"
+	schoolerrors "schoolbackend/errors"
 	"schoolbackend/token"
 	"strings"
 
@@ -11,13 +13,13 @@ func AuthenticateParent() gin.HandlerFunc {
     return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("Authorization")
 		if clientToken ==""{
-			c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "Authorization token is required"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": schoolerrors.ErrorUnauthorizedAccess.Message})			
 			c.Abort()
 			return
 		}
 		splitToken := strings.Split(clientToken, "Bearer ")
 		if len(splitToken) != 2 {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
+            c.JSON(http.StatusUnauthorized, gin.H{"error": schoolerrors.ErrorInvalidHeaderFormat.Message})
             c.Abort()
             return
         }
@@ -25,13 +27,13 @@ func AuthenticateParent() gin.HandlerFunc {
 
         claims, err := token.ValidateToken(clientToken)
         if err != "" {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+            c.JSON(http.StatusUnauthorized, gin.H{"error": schoolerrors.ErrorInvalidToken.Message})
             c.Abort()
             return
         }
 		userRole := claims.Role 
-        if userRole != "student" {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": "Only Students have this access"})
+        if userRole != "parent" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": schoolerrors.ErrorParentAccessOnly.Message})
             c.Abort()
             return
         }
